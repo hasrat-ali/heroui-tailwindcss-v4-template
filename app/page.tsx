1b2a10a1609a2fa77b22884c74ae9767053039a1
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2025-05-31 21:49:16 +0530

    fix: Adjust some ui, move themeswitcher on top

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@ import { ThemeSwitcher } from "@/components/ThemeSwitcher";
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20">
+      <main className="row-start-2 flex flex-col items-center gap-8">
+        <ThemeSwitcher />
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -13,10 +14,10 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm/6 sm:text-left">
+        <ol className="list-inside list-decimal text-center text-sm/6">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
-            <code className="rounded bg-black/[.05] px-1 py-0.5 font-[family-name:var(--font-geist-mono)] font-semibold dark:bg-white/[.06]">
+            <code className="rounded bg-black/[.05] px-1 py-0.5 font-semibold">
               app/page.tsx
             </code>
             .
@@ -25,9 +26,7 @@ export default function Home() {
             Save and see your changes instantly.
           </li>
         </ol>
-
         <div className="flex flex-col items-center gap-4 sm:flex-row">
-          <ThemeSwitcher />
           <a
             className="bg-foreground text-background flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent px-4 text-sm font-medium transition-colors hover:bg-[#383838] sm:h-12 sm:w-auto sm:px-5 sm:text-base dark:hover:bg-[#ccc]"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
